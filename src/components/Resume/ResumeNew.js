44a19cfef46a9cf1e4b81044cf2c8de590b3e031
Kareem Al-Skafi <80727181+kareemalskafi@users.noreply.github.com>
2023-06-30 16:18:07 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-//import pdf from "../../Assets/../Assets/Abdalkareem_AlSkafi-Full-Stack-Developer.pdf";
 import pdf from "../../Assets/Abdalkareem_AlSkafi-Full-Stack-Developer.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
