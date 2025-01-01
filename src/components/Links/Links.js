a18e6fcb915964e09ce3fbcc32b1cb7d6bc40cc9
kareem alskafi <kareemalskafi@gmail.com>
2025-01-01 16:39:12 +0300

    Edit Content

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -150,9 +150,248 @@ function Links() {
           )}
         </div>
       ) : authState === "hashedPassword01" ? (
-        <Content01 />
+        <Container className="home-content">
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={4} className="home-header">
+              <h1 className="hyper-link">Content 01 First Row Ka</h1>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://google.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                1. Google
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://facebook.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                2. Facebook
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://www.youtube.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                3. Youtube
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://linkedin.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                4. Linkedin
+              </Button>
+            </Col>
+
+            <Col md={4} className="home-header">
+              <h1 className="hyper-link">Content 01 Second Row Ka</h1>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://google.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                1. Google
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://facebook.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                2. Facebook
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://www.youtube.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                3. Youtube
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://linkedin.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                4. Linkedin
+              </Button>
+            </Col>
+
+            <Col md={4} className="home-header">
+              <h1 className="hyper-link">Content 01 Third Row Ka</h1>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://google.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                1. Google
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://facebook.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                2. Facebook
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://www.youtube.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                3. Youtube
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://linkedin.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                4. Linkedin
+              </Button>
+            </Col>
+          </Row>
+          <Row
+            style={{
+              justifyContent: "flex-end",
+              paddingBottom: "10px",
+              paddingTop: "50px",
+            }}
+          ></Row>
+        </Container>
       ) : authState === "hashedPassword02" ? (
-        <Content02 />
+        <Container className="home-content">
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={4} className="home-header">
+              <h1 className="hyper-link">Content 02 First Row Kaa</h1>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://google.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                1. Google
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://facebook.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                2. Facebook
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://www.youtube.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                3. Youtube
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://linkedin.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                4. Linkedin
+              </Button>
+            </Col>
+
+            <Col md={4} className="home-header">
+              <h1 className="hyper-link">Content 02 Second Row Kaa</h1>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://google.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                1. Google
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://facebook.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                2. Facebook
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://www.youtube.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                3. Youtube
+              </Button>
+
+              <Button
+                variant="primary"
+                className="like-btn"
+                href="https://linkedin.com/"
+                target="_blank"
+                style={{ margin: "7px" }}
+              >
+                4. Linkedin
+              </Button>
+            </Col>
+          </Row>
+
+          <Row
+            style={{
+              justifyContent: "flex-end",
+              paddingBottom: "10px",
+              paddingTop: "50px",
+            }}
+          ></Row>
+        </Container>
       ) : null}
 
       <Particle />
